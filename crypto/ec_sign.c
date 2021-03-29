589d87ec0f3c540f854164238f98752e0827069e
cybernuki <1164@holbertonschool.com>
2021-03-29 01:50:19 +0000

    betty

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -11,16 +11,16 @@
  * Return: a pointer to the signature buffer upon success (sig->sig)
  */
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-                 sig_t *sig)
+				 sig_t *sig)
 {
-    unsigned int len;
+	unsigned int len;
 
-    if (!key || !msg || !sig)
-        return (NULL);
-    len = sig->len;
-    if (ECDSA_sign(0, msg, msglen, sig->sig, &len,
-                   (EC_KEY *)key) != 1)
-        return (NULL);
-    sig->len = len;
-    return (sig->sig);
+	if (!key || !msg || !sig)
+		return (NULL);
+	len = sig->len;
+	if (ECDSA_sign(0, msg, msglen, sig->sig, &len,
+				   (EC_KEY *)key) != 1)
+		return (NULL);
+	sig->len = len;
+	return (sig->sig);
 }
