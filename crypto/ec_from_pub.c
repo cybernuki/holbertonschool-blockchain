48c6fb50caf12336c53e2f2ec1c0e6a656062d93
cybernuki <1164@holbertonschool.com>
2021-03-29 01:08:12 +0000

    betty

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -22,7 +22,9 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 		EC_KEY_free(key);
 		return (NULL);
 	}
-	if (!EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN, NULL) || !EC_KEY_set_public_key(key, point))
+	if (!EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN,
+							NULL) ||
+		!EC_KEY_set_public_key(key, point))
 	{
 		EC_KEY_free(key);
 		EC_POINT_free(point);
