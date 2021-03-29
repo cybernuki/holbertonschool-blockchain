7e91596c37fa3f7c6c62fc10d4725b17defdd22e
cybernuki <1164@holbertonschool.com>
2021-03-29 02:02:11 +0000

    betty

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -9,10 +9,10 @@
  * Return: 1 if the signature is correct, 0 otherwise.
  */
 int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
-              sig_t const *sig)
+			  sig_t const *sig)
 {
-    if (!key || !msg || !sig)
-        return (0);
-    return (ECDSA_verify(0, msg, msglen, sig->sig,
-                         sig->len, (EC_KEY *)key) == 1);
+	if (!key || !msg || !sig)
+		return (0);
+	return (ECDSA_verify(0, msg, msglen, sig->sig,
+						 sig->len, (EC_KEY *)key) == 1);
 }
