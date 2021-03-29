4a46a8ac74b2cc4e6099e6a65a608d7d5a63d83f
cybernuki <1164@holbertonschool.com>
2021-03-29 00:37:01 +0000

    fix

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -6,15 +6,15 @@
  */
 EC_KEY *ec_create(void)
 {
-    EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
+	EC_KEY *key = EC_KEY_new_by_curve_name(EC_CURVE);
 
-    if (!key)
-        return (NULL);
+	if (!key)
+		return (NULL);
 
-    if (!EC_KEY_generate_key(key))
-    {
-        EC_KEY_free(key);
-        return (NULL);
-    }
-    return (key);
+	if (!EC_KEY_generate_key(key))
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	return (key);
 }
