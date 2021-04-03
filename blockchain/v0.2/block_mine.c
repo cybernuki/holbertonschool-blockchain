4c29cb933d192645b1559ba3bb71eb5db3627ab9
cybernuki <1164@holbertonschool.com>
2021-04-03 21:06:34 +0000

    block mine

---
+++ blockchain/v0.2/block_mine.c
@@ -0,0 +1,17 @@
+#include "blockchain.h"
+
+/**
+ * block_mine - block_mine mines a new block inside the blockchain
+ * @block: is the block to be mined
+ */
+void block_mine(block_t *block)
+{
+    uint64_t nonce = 0;
+
+    do
+    {
+        block->info.nonce = nonce;
+        block_hash(block, block->hash);
+        nonce++;
+    } while (!hash_matches_difficulty(block->hash, block->info.difficulty));
+}
