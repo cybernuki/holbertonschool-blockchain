a0fac07a676a002126247f8e1d3ca7bf0cb4a9eb
cybernuki <1164@holbertonschool.com>
2021-04-03 21:24:33 +0000

    fix

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -22,6 +22,8 @@
 
 #define HBLK_MAGIC "HBLK"
 #define HBLK_VERSION "0.2"
+#define BLOCK_GENERATION_INTERVAL 1
+#define DIFFICULTY_ADJUSTMENT_INTERVAL 5
 
 /**
  * struct blockchain_s - Blockchain structure
@@ -122,4 +124,5 @@ int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 							uint32_t difficulty);
 void block_mine(block_t *block);
+uint32_t blockchain_difficulty(blockchain_t const *blockchain);
 #endif
