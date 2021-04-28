a980802c3083c02a8b8c6f41fa74ff31153f5d76
cybernuki <1164@holbertonschool.com>
2021-04-28 03:36:04 +0000

    fix

--- blockchain/v0.3/blockchain_create.c
+++ blockchain/v0.3/blockchain_create.c
@@ -14,10 +14,14 @@ blockchain_t *blockchain_create(void)
 	blockchain_t *chain = calloc(1, sizeof(*chain));
 	block_t *block = calloc(1, sizeof(*block));
 	llist_t *list = llist_create(MT_SUPPORT_TRUE);
+	llist_t *unspent = llist_create(MT_SUPPORT_TRUE);
 
-	if (!chain || !block || !list)
+	if (chain == NULL || block == NULL || list == NULL || unspent == NULL)
 	{
-		free(chain), free(block), llist_destroy(list, 1, NULL);
+		free(chain);
+		free(block);
+		llist_destroy(list, 1, NULL);
+		llist_destroy(unspent, 1, NULL);
 		return (NULL);
 	}
 
@@ -28,9 +32,12 @@ blockchain_t *blockchain_create(void)
 
 	if (llist_add_node(list, block, ADD_NODE_FRONT))
 	{
-		free(chain), free(block), llist_destroy(list, 1, NULL);
+		free(chain);
+		free(block);
+		llist_destroy(list, 1, NULL);
 		return (NULL);
 	}
 	chain->chain = list;
+	chain->unspent = unspent;
 	return (chain);
 }
